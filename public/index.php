8774d2b95c6cb27ea9fe3e800682c23643905d0e
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-13 15:13:02 +0300

    Delete our check

--- public/index.php
+++ public/index.php
@@ -4,9 +4,4 @@ require_once '../vendor/autoload.php';
 
 use Mailer\Transport\Message;
 
-Message::send(
-    'Registration',
-    'confirmOfRegistr',
-    ['title' => 'Registration'],
-    'karlovalesa06@gmail.com'
-);
+Message::send();
