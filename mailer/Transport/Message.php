7d91172a46026b316392cfa8e3f224f845774b30
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-15 15:38:39 +0300

    Add logger

--- mailer/Transport/Message.php
+++ mailer/Transport/Message.php
@@ -3,6 +3,8 @@
 namespace Mailer\Transport;
 
 use Config\Config;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 class Message
 {
@@ -13,12 +15,20 @@ class Message
         $mailer = new MailTransport($transport);
         $message = new \Swift_Message();
 
-        $message->setSubject($subject);
-        $message->setBody((new Message())->template($templateName, $vars), 'text/html');
-        $message->setTo($to);
-        $message->setFrom($config['services']['gmail']['username']);
+        $log = new Logger('mailer');
+        $log->pushHandler(new StreamHandler($config['logs']['path'], Logger::INFO));
+        $log->info('Sending', ['subject' => $subject, 'to' => $to]);
 
-        $mailer->getMailer()->send($message);
+        try {
+            $message->setSubject($subject);
+            $message->setBody((new Message())->template($templateName, $vars), 'text/html');
+            $message->setTo($to);
+            $message->setFrom($config['services']['gmail']['username']);
+            $mailer->getMailer()->send($message);
+        } catch (\Throwable $e) {
+            $log->pushHandler(new StreamHandler($config['logs']['path'], Logger::WARNING));
+            $log->warning('There isn`t enough data for sending');
+        }
     }
 
     private function template($templateName, $vars): string
